78fc9b28b88c640015427c950061588fc4de5ad7
Zachary Hayes <zachary.j.hayes@gmail.com>
2019-11-25 21:27:58 -0600

    Added scenes

--- SceneData.cpp
+++ SceneData.cpp
@@ -18,14 +18,14 @@ void SceneData::initializeScenes()
     Scene* sceneB = new Scene("A ghost guards the upstairs hallway and will not let you pass. \"I will haunt these halls until my murder is solved.\""); // Upstairs hallways
     Scene* sceneC = new Scene("A chef is cutting carrots, she pays you no attention."); // Kitchen
     Scene* sceneD = new Scene("This must be someone's office. You get goosebumps the moment you enter. It's lit by a single candle sitting on the antique desk. How long has that candle been burning? The dusty bookshelf looks like it hasn't been touched in ages. There's a stain in the wood floor where carpet had been torn up. Blood?"); // Den;
-    Scene* sceneE = new Scene(""); // Upstairs hallway 2
+    Scene* sceneE = new Scene("The upstairs hallway has three doors. You can see that the middle door has a padlock; It requires a key to open. On the ceiling is an access door, presumably to the attic, but it's too high up to reach."); // Upstairs hallway 2
     Scene* sceneF = new Scene("The desk is an antique. A stack of newspaper clippings date back fifty years."); // Desk
     Scene* sceneG = new Scene("Hundreds of old books line the bookshelf, where does one even begin?"); // Bookshelf
     Scene* sceneH = new Scene("The hidden room is like a dungeon. It's empty, other than a pedestal in the middle of the room holding an enormous diamond, your instincts tell you not to touch it. A skeleton lays beneath the pedestal clutching a journal."); // Hidden room
-    Scene* sceneI;
-    Scene* sceneJ;
-    Scene* sceneK;
-    Scene* sceneL;
+    Scene* sceneI = new Scene("You're met with a vicious looking demon. \"You've come to challenge me?\" he cackles, \"Surrender your soul, and I will make your death painless.\""); // Master's Room
+    Scene* sceneJ = new Scene("The bedroom is nothing unusual. A bed, a closet, and a vanity table complete with a chair."); // Bedroom
+    Scene* sceneK = new Scene("You enter the bathroom, and a man is singing in the shower. His clothes are hung up, along with a master chef's hat."); // Bathroom
+    Scene* sceneL = new Scene("As you raise yourself into the attic, you're startled by a figure standing right in front of you. It's just a mannequin. The attic is full of old antiques and dusty boxes. The ghost of a child appears. He holds up a key, \"What will you trade me?\""); // Attic
     
     firstScene = sceneA;
     
@@ -60,7 +60,7 @@ void SceneData::initializeScenes()
     // SCENE B - UPSTAIRS HALLWAY
     Choice* choiceB1 = new Choice();
     choiceB1->description = "Show the ghost his killer's journal.";
-    choiceB1->response = "The ghost weeps, \"Finally, I can rest in peace.\"";
+    choiceB1->response = "The ghost weeps, \"Finally, I can rest in peace. Be forewarned, a great evil lives on your path forward.\" Then the ghost's expression changes, and he looks at you like you're a complete stranger.";
     choiceB1->requiresItem = true;
     choiceB1->requiredItem = "journal";
     choiceB1->changesScenes = true;
@@ -86,7 +86,7 @@ void SceneData::initializeScenes()
     choiceC1->description = "*ahem* \"I need that knife.\"";
     choiceC1->response = "The chef gasps, \"Of course Master Chef.\" She hands over her cutting knife, then unsheaths another knife from her belt and continues chopping carrots.";
     choiceC1->requiresItem = true;
-    choiceC1->requiredItem = "master chef's hat";
+    choiceC1->requiredItem = "chef's hat";
     choiceC1->dropsLoot = true;
     choiceC1->loot = "knife";
     sceneC->addChoice(choiceC1);
@@ -128,6 +128,44 @@ void SceneData::initializeScenes()
     sceneD->addChoice(choiceD3);
     
     // SCENE E - UPSTAIRS HALLWAY 2
+    Choice* choiceE1 = new Choice();
+    choiceE1->description = "Unlock the padlocked door.";
+    choiceE1->response = "The key fits the lock perfectly. You remove the padlock, and push the door open.";
+    choiceE1->requiresItem = true;
+    choiceE1->requiredItem = "key";
+    choiceE1->changesScenes = true;
+    choiceE1->nextScene = sceneI;
+    sceneE->addChoice(choiceE1);
+    
+    Choice* choiceE2 = new Choice();
+    choiceE2->description = "See what's behind the left door.";
+    choiceE2->response = "You open the door partially and peak inside, it's an empty bedroom. The door opens itself the rest of the way.";
+    choiceE2->changesScenes = true;
+    choiceE2->nextScene = sceneJ;
+    sceneE->addChoice(choiceE2);
+
+    Choice* choiceE3 = new Choice();
+    choiceE3->description = "See what's behind the right door.";
+    choiceE3->response = "Steam bellows out of the doorway as you open the door. It's a bathroom.";
+    choiceE3->changesScenes = true;
+    choiceE3->nextScene = sceneK;
+    sceneE->addChoice(choiceE3);
+
+    Choice* choiceE4 = new Choice();
+    choiceE4->description = "Use the chair to reach the attic door.";
+    choiceE4->response = "You place the chair beneath the attic door, and climb up by pushing the attic access door aside and pulling yourself up.";
+    choiceE4->requiresItem = true;
+    choiceE4->requiredItem = "chair";
+    choiceE4->changesScenes = true;
+    choiceE4->nextScene = sceneL;
+    sceneE->addChoice(choiceE4);
+    
+    Choice* choiceE5 = new Choice();
+    choiceE5->description = "Go back downstairs.";
+    choiceE5->response = "You try to ignore the ghost, who has not kept his promise to stop haunting the hallway, as you make your way back downstairs to the foyer";
+    choiceE5->changesScenes = true;
+    choiceE5->nextScene = sceneA;
+    sceneE->addChoice(choiceE5);
     
     // SCENE F - DESK
     Choice* choiceF1 = new Choice();
@@ -218,6 +256,95 @@ void SceneData::initializeScenes()
     choiceH4->changesScenes = true;
     choiceH4->nextScene = sceneD;
     sceneH->addChoice(choiceH4);
+    
+    // SCENE I - MASTER BEDROOM
+    Choice* choiceI1 = new Choice();
+    choiceI1->description = "Stab the demon.";
+    choiceI1->response = "The demon howls, \"Why! Why must I be weak to common cutlery!\" and he is sucked into a portal straight to hell. You've defeated the demon who trapped the ghosts in this house, their souls can rest now thanks to you. You leave the knife, and return to the foyer. A job well-done. Another demon vanguished. As you walk into the sunset, you wonder what your next adventure will be.";
+    choiceI1->requiresItem = true;
+    choiceI1->requiredItem = "knife";
+    choiceI1->isExit = true;
+    sceneI->addChoice(choiceI1);
+    
+    Choice* choiceI2 = new Choice();
+    choiceI2->description = "Surrender your soul.";
+    choiceI2->response = "The demon laughs, and consumes you in fire. He kept his word, it was painless.";
+    choiceI2->isExit = true;
+    sceneI->addChoice(choiceI2);
+    
+    Choice* choiceI3 = new Choice();
+    choiceI3->description = "Run for your life.";
+    choiceI3->response = "You turn and run out the door, back into the hallway. You'll return when you're more prepared for battle.";
+    choiceI3->changesScenes = true;
+    choiceI3->nextScene = sceneE;
+    sceneI->addChoice(choiceI3);
+    
+    // SCENE J - BEDROOM
+    Choice* choiceJ1 = new Choice();
+    choiceJ1->description = "Take a closer look at the bed.";
+    choiceJ1->response = "Looks like it's been slepted in recently.";
+    sceneJ->addChoice(choiceJ1);
+    
+    Choice* choiceJ2 = new Choice();
+    choiceJ2->description = "Examine the vanity table.";
+    choiceJ2->response = "Apart from the attractive person in the mirror, nothing sticks out about the vanity table. The chair, however, might come in handy.";
+    choiceJ2->dropsLoot = true;
+    choiceJ2->loot = "chair";
+    sceneJ->addChoice(choiceJ2);
+    
+    Choice* choiceJ3 = new Choice();
+    choiceJ3->description = "Look inside the closet.";
+    choiceJ3->response = "There is literally a skeleton in this closet.";
+    sceneJ->addChoice(choiceJ3);
+    
+    Choice* choiceJ4 = new Choice();
+    choiceJ4->description = "Go back.";
+    choiceJ4->response = "You leave the bedroom, and go back into the hallway.";
+    choiceJ4->changesScenes = true;
+    choiceJ4->nextScene = sceneE;
+    sceneJ->addChoice(choiceJ4);
+    
+    // SCENE K - BATHROOM
+    Choice* choiceK1 = new Choice();
+    choiceK1->description = "Flush the toilet.";
+    choiceK1->response = "The man in the shower screeches, \"Ahhh-ooo! That's cold!\"";
+    sceneK->addChoice(choiceK1);
+    
+    Choice* choiceK2 = new Choice();
+    choiceK2->description = "Steal the chef's hat.";
+    choiceK2->response = "You remove the master chef's hat hanging from the towel rack, and put it on your own head. You look to check yourself in the mirror, but it's sadly too foggy.";
+    choiceK2->dropsLoot = true;
+    choiceK2->loot = "chef's hat";
+    sceneK->addChoice(choiceK2);
+    
+    Choice* choiceK3 = new Choice();
+    choiceK3->description = "Leave bathroom.";
+    choiceK3->response = "You shut the door on your way out, muffling the showering man's singing.";
+    choiceK3->changesScenes = true;
+    choiceK3->nextScene = sceneE;
+    sceneK->addChoice(choiceK3);
+    
+    // SCENE L - ATTIC
+    Choice* choiceL1 = new Choice();
+    choiceL1->description = "Trade the diamond for the key.";
+    choiceL1->response = "The ghost child accepts, and hands you the key in exchange for the diamond. You begin to wonder if you were bamboozled. \"But be warned,\" moans the ghost child, \"The demon who lives in the Master's Room will not be easy to defeat. You'll want to find a weapon before challenging him.\"";
+    choiceL1->dropsLoot = true;
+    choiceL1->loot = "key";
+    sceneL->addChoice(choiceL1);
+    
+    Choice* choiceL2 = new Choice();
+    choiceL2->description = "Put chef's hat on the mannequin.";
+    choiceL2->response = "The faceless mannequin gives you a nod of approval.";
+    choiceL2->requiresItem = true;
+    choiceL2->requiredItem = "knife";
+    sceneL->addChoice(choiceL2);
+    
+    Choice* choiceL3 = new Choice();
+    choiceL3->description = "Climb down.";
+    choiceL3->response = "The chair wobbles under your feet as you climb down from the attic. You hear the child's ghostly laughter above.";
+    choiceL3->changesScenes = true;
+    choiceL3->nextScene = sceneE;
+    sceneL->addChoice(choiceL3);
 }
 
 Scene* SceneData::getFirstScene()
